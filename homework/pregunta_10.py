169d953702073f445d78115e610f20b3f7f9c67d
Laurehc <111622067+Laurehc@users.noreply.github.com>
2025-02-21 03:11:15 -0500

    Lab 02
    Pandas

--- homework/pregunta_10.py
+++ homework/pregunta_10.py
@@ -21,12 +21,13 @@ def pregunta_10():
     D                   1:2:3:5:5:7
     E   1:1:2:3:3:4:5:5:5:6:7:8:8:9
     """
-    df = pd.read_csv("files/input/tbl0.tsv", sep="\t")
-    
-    # Agrupar por 'c1' y concatenar los valores de 'c2' en un solo string separado por ':'
-    tabla = df.groupby("c1")["c2"].apply(lambda x: ":".join(map(str, sorted(x)))).reset_index()
+    df = pd.read_csv("files/input/tbl0.tsv", sep="\t")   
 
-    return tabla  # Retornamos el DataFrame con la nueva estructura
+    # Ordeanar c2 en la tabla   
+    tabla = df.groupby('c1')['c2'].apply(lambda x: ':'.join(map(str, sorted(x)))).reset_index()        
+    tabla.set_index('c1', inplace=True)
+    
+    return tabla  # Retorna el DataFrame sin resetear el índice
 
 # Llamar a la función y mostrar el resultado
 print(pregunta_10())
